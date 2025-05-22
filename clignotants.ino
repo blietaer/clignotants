0410df2c62f5fa755d8dcd1ba6532f352ae3b342
Benoit LIETAER <benoit.lietaer@gmail.com>
2025-05-22 14:56:12 +0200

    Better IRQ pins

--- clignotants.ino
+++ clignotants.ino
@@ -8,48 +8,48 @@
 **************************************************************************/
 
 // STRIP PINOUT
-#define PIN_RIGHT 4  
-#define PIN_FRONT 7  
-#define PIN_LEFT 6  
+const byte PIN_RIGHT = 20; 
+const byte PIN_FRONT = 19;  
+const byte PIN_LEFT =  18;  
 
 // BUTTONS PINOUT
-#define PIN_AVA_BUT 9  
-#define PIN_WRN_BUT 10  
-#define PIN_ARR_BUT 12  
-#define PIN_GAU_BUT 13  
-#define PIN_DRO_BUT 14  
-#define PIN_GAY_BUT 18  
-#define PIN_RED_BUT 19  
+const byte PIN_AVA_BUT =  9;  
+const byte PIN_WRN_BUT =  5; 
+const byte PIN_ARR_BUT =  3; 
+const byte PIN_GAU_BUT =  7;  
+const byte PIN_DRO_BUT =  8;  
+const byte PIN_GAY_BUT =  6;  
+const byte PIN_RED_BUT = 16;  
 
 
-#define PIN_LED_R 15  
-#define PIN_LED_L 16  
-#define PIN_RELAY 17  
+const byte PIN_LED_R = 13;  
+const byte PIN_LED_L = 4;  
+const byte PIN_RELAY = 15;  
 
+const byte NUM_PIX_FRONT =  40;
+const byte NUM_PIX_BACK  =  20;
 
-#define NUM_PIX_FRONT 40
-#define NUM_PIX_BACK  20
-
-#define SHORT_DELAY 10
-#define LONG_DELAY 250
+const byte  SHORT_DELAY = 10;
+const byte  LONG_DELAY = 250;
 
 Adafruit_NeoPixel strip_left(NUM_PIX_BACK, PIN_LEFT, NEO_RGBW + NEO_KHZ800);
 Adafruit_NeoPixel strip_right(NUM_PIX_BACK, PIN_RIGHT, NEO_RGBW + NEO_KHZ800);
 Adafruit_NeoPixel strip_front(NUM_PIX_FRONT,PIN_FRONT, NEO_RGBW + NEO_KHZ800);
 
-int buttonState = 0;
-int status_front_light = 0;
-int status_back_light = 0;
-int status_left_light = 0;
-int status_right_light = 0;
-int status_warn_light = 0;
+volatile byte buttonState = 0;
+volatile byte status_front_light = 0;
+volatile byte status_back_light = 0;
+volatile byte status_left_light = 0;
+volatile byte status_right_light = 0;
+volatile byte status_warn_light = 0;
+volatile byte status_gay_light = 0;
 
 /**************************************************************************
  * S E T U P
 **************************************************************************/
 void setup() {
 
-  Serial.begin(9600);
+  //Serial.begin(9600);
 
   pinMode(PIN_RELAY, OUTPUT);
   pinMode(PIN_LED_R, OUTPUT);
@@ -63,10 +63,31 @@ void setup() {
   pinMode(PIN_GAY_BUT, INPUT_PULLUP);
   pinMode(PIN_RED_BUT, INPUT_PULLUP);
 
+  attachInterrupt(PIN_WRN_BUT, irq_warn, FALLING);
+  attachInterrupt(PIN_GAU_BUT, irq_left, FALLING);
+  attachInterrupt(PIN_DRO_BUT, irq_right, FALLING);
+  attachInterrupt(PIN_GAY_BUT, irq_gay, FALLING);
+
   strip_front.begin();
   strip_left.begin();
   strip_right.begin();
 
+  clear_them_all();
+}
+
+/**************************************************************************
+ *  C L E A R 
+**************************************************************************/
+void clear_them_all() {
+  buttonState = 0;
+  status_left_light = 0;
+  status_right_light = 0;
+  status_warn_light = 0;
+  status_gay_light = 0;
+  status_gay_light = 0;
+  status_front_light = 0;
+  status_back_light = 0;
+ 
   strip_front.clear();
   strip_left.clear();
   strip_right.clear();
@@ -76,98 +97,133 @@ void setup() {
  * L O O P
 **************************************************************************/
 void loop() {
-
-  if (status_left_light) {
-    // clignot_left();
-    Serial.println("LEFT IS STILL ON ! ");
-    delay(LONG_DELAY);      
-  }
-
-  if (status_right_light) {
-    // clignot_right();
-    Serial.println("RIGHT IS STILL ON ! ");
-    delay(LONG_DELAY);      
-  }
-
   // F R O N T 
   buttonState = digitalRead(PIN_AVA_BUT);
   if (buttonState == LOW) {
 
     if (status_front_light) {
-      Serial.println("FRONT PRESSED: OFF ! ");
+      //Serial.println("FRONT PRESSED: OFF ! ");
       status_front_light = false;
       front_light(status_front_light);
     } else {
-      Serial.println("FRONT PRESSED: ON ! ");
+      //Serial.println("FRONT PRESSED: ON ! ");
       status_front_light = true;
       front_light(status_front_light);
     }
   }
-  
   // B A C K 
   buttonState = digitalRead(PIN_ARR_BUT);
   if (buttonState == LOW) {
     if (status_back_light) {
-      Serial.println("BACK PRESSED: OFF ! ");
+      //Serial.println("BACK PRESSED: OFF ! ");
       status_back_light = false;
       back_light(status_back_light);
     } else {
-      Serial.println("BACK PRESSED: ON ! ");
+      //Serial.println("BACK PRESSED: ON ! ");
       status_back_light = true;
       back_light(status_back_light);
     }
 
   }
 
-  // L E F T 
-  buttonState = digitalRead(PIN_GAU_BUT);
-  if (buttonState == LOW) {
-    if (status_left_light) {
-      Serial.println("LEFT PRESSED: OFF ! ");
-      status_left_light = false;
-    } else {
-      Serial.println("LEFT PRESSED: ON ! ");
-      status_left_light = true;
-    }
-
-  }
-
-  // R I G H T
-  buttonState = digitalRead(PIN_DRO_BUT);
-  if (buttonState == LOW) {
-    if (status_right_light) {
-    Serial.println("RIGHT PRESSED: OFF ! ");
-      status_right_light = false;
-    } else {
-    Serial.println("RIGHT PRESSED: ON ! ");
-      status_right_light = true;
-    }
-  }
-
-  // W A R N 
-  buttonState = digitalRead(PIN_WRN_BUT);
-  if (buttonState == LOW) {
-    Serial.println("WARN PRESSED ! ");
-    clignot_warn();
-  }
-
-  // G A Y 
-  buttonState = digitalRead(PIN_GAY_BUT);
-  if (buttonState == LOW) {
-    Serial.println("GAY PRESSED ! ");
-    clignot_right();
-  }
-
   // R E D
   buttonState = digitalRead(PIN_RED_BUT);
   if (buttonState == LOW) {
-    Serial.println("RED PRESSED ! ");
-    theaterChase(strip_front.Color(0, 255,0), 2);
-    theaterChase(strip_front.Color(0, 0, 255), 2);
-    theaterChase(strip_front.Color(0, 255,0), 2);
-    theaterChase(strip_front.Color(0, 0, 255), 2);
+    //Serial.println("RED PRESSED ! ");
+    cop_flash(strip_front.Color(0, 255,0), 1);
+    cop_flash(strip_front.Color(0, 0,0), 1);
+    cop_flash(strip_front.Color(0, 0, 255), 1);
+    cop_flash(strip_front.Color(0, 0,0), 1);
+    cop_flash(strip_front.Color(0, 255,0), 1);
+    cop_flash(strip_front.Color(0, 0,0), 1);
+    cop_flash(strip_front.Color(0, 0, 255), 1);
+    strip_front.clear();
+    strip_left.clear();
+    strip_right.clear();
+    strip_front.fill(strip_front.Color(0, 0, 0, 0));
+    strip_left.fill(strip_front.Color(0, 0, 0, 0));
+    strip_right.fill(strip_front.Color(0, 0, 0, 0));
+    strip_front.show();
+    strip_left.show();
+    strip_right.show();
   }
 
+  if (status_warn_light) {
+    clignot_warn();
+    delay(LONG_DELAY);      
+  }
+
+  if (status_right_light) {
+    clignot_right();
+    delay(LONG_DELAY);      
+  }
+
+  if (status_left_light) {
+    clignot_left();
+    delay(LONG_DELAY);      
+  }
+  delay(100);
+}
+/**************************************************************************
+ * W A R N  I S R 
+**************************************************************************/
+void irq_warn() {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > LONG_DELAY)
+  {
+    if (status_warn_light) {
+     clear_them_all(); 
+    } else {
+     status_warn_light = true; 
+    }
+  }
+}
+/**************************************************************************
+ * L E F T   I S R 
+**************************************************************************/
+void irq_left() {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > LONG_DELAY)
+  {
+    if (status_left_light) {
+     clear_them_all(); 
+    } else {
+     status_left_light = true; 
+    }
+  }
+}
+/**************************************************************************
+ * L E F T   I S R 
+**************************************************************************/
+void irq_right() {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > LONG_DELAY)
+  {
+    if (status_right_light) {
+     clear_them_all(); 
+    } else {
+     status_right_light = true; 
+    }
+  }
+}
+/**************************************************************************
+ * G A Y  I S R 
+**************************************************************************/
+void irq_gay() {
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > LONG_DELAY)
+  {
+    status_gay_light = !status_gay_light;
+    if (status_gay_light) {
+      gay_parade();
+    } else {
+      clear_them_all;
+    }
+  }
 }
 /**************************************************************************
  * B L I N K    R I G H T
@@ -248,64 +304,74 @@ void clignot_warn() {
 }
 
 /**************************************************************************
- * T H E A T R E   C H A S E 
+ * C O P   F L A S H 
 **************************************************************************/
-void theaterChase(uint32_t color, int wait) {
+void cop_flash(uint32_t color, int wait) {
   for(int a=0; a<10; a++) {  // Repeat 10 times...
     for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
       strip_front.clear();         //   Set all pixels in RAM to 0 (off)
+      strip_left.clear();         //   Set all pixels in RAM to 0 (off)
+      strip_right.clear();         //   Set all pixels in RAM to 0 (off)
       // 'c' counts up from 'b' to end of strip in steps of 3...
       for(int c=b; c<strip_front.numPixels(); c += 3) {
         strip_front.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+        strip_left.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+        strip_right.setPixelColor(c, color); // Set pixel 'c' to value 'color'
       }
       strip_front.show(); // Update strip with new contents
+      strip_right.show(); // Update strip with new contents
+      strip_left.show(); // Update strip with new contents
       delay(wait);  // Pause for a moment
     }
   }
 }
+/**************************************************************************
+ * W H E E L   O F   C O L O R S 
+**************************************************************************/
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+    return strip_front.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } 
+  else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip_front.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } 
+  else {
+    WheelPos -= 170;
+    return strip_front.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
 
 /**************************************************************************
- * W H I T E   O V E R   R A I N B O W
+ * G A Y   P A R A D E
 **************************************************************************/
-void whiteOverRainbow(int whiteSpeed, int whiteLength) {
+void gay_parade() {
+  uint16_t i, j;
 
-  if(whiteLength >= strip_front.numPixels()) whiteLength = strip_front.numPixels() - 1;
-
-  int      head          = whiteLength - 1;
-  int      tail          = 0;
-  int      loops         = 3;
-  int      loopNum       = 0;
-  uint32_t lastTime      = millis();
-  uint32_t firstPixelHue = 0;
-
-  for(;;) { // Repeat forever (or until a 'break' or 'return')
-    for(int i=0; i<strip_front.numPixels(); i++) {  // For each pixel in strip_front...
-      if(((i >= tail) && (i <= head)) ||      //  If between head & tail...
-         ((tail > head) && ((i >= tail) || (i <= head)))) {
-        strip_front.setPixelColor(i, strip_front.Color(0, 0, 0, 255)); // Set white
-      } else {                                             // else set rainbow
-        int pixelHue = firstPixelHue + (i * 65536L / strip_front.numPixels());
-        // strip_front.setPixelColor(i, strip_front.gamma8(strip_front.ColorHSV(pixelHue)));
-      }
+  for(j=0; j<256; j++) {
+    for(i=0; i< strip_front.numPixels(); i++) {
+      strip_front.setPixelColor(i, Wheel((i*1+j) & 255));
+    }
+    for(i=0; i< strip_left.numPixels(); i++) {
+      strip_left.setPixelColor(i, Wheel((i*1+j) & 255));
+    }
+    for(i=0; i< strip_right.numPixels(); i++) {
+      strip_right.setPixelColor(i, Wheel((i*1+j) & 255));
     }
 
-    strip_front.show(); // Update strip with new contents
-    // There's no delay here, it just runs full-tilt until the timer and
-    // counter combination below runs out.
-
-    firstPixelHue += 40; // Advance just a little along the color wheel
-
-    if((millis() - lastTime) > whiteSpeed) { // Time to update head/tail?
-      if(++head >= strip_front.numPixels()) {      // Advance head, wrap around
-        head = 0;
-        if(++loopNum >= loops) return;
-      }
-      if(++tail >= strip_front.numPixels()) {      // Advance tail, wrap around
-        tail = 0;
-      }
-      lastTime = millis();                   // Save time of last movement
+    strip_front.show();
+    strip_left.show();
+    strip_right.show();
+    if  (!status_gay_light) {
+      clear_them_all();
+      strip_right.fill(strip_front.Color(0, 0, 0, 0));
+      strip_right.show();
+      strip_front.clear();
+      return;
     }
+    delay(SHORT_DELAY);
   }
+
 }
 /**************************************************************************
  * B A C K   L I G H T
@@ -314,18 +380,18 @@ void back_light(bool enabled ) {
   if (enabled) {
     
     for(int j=0; j<256; j++) { // Ramp up from 0 to 255
-      strip_right.fill(strip_right.Color(0, 255, 0, strip_right.gamma8(j)));
+      strip_right.fill(strip_right.Color(0, 254, 0, strip_right.gamma8(j)));
       strip_right.show();
-      strip_left.fill(strip_left.Color(0, 255, 0, strip_left.gamma8(j)));
+      strip_left.fill(strip_left.Color(0, 254, 0, strip_left.gamma8(j)));
       strip_left.show();
     }
 
   } else {
 
     for(int j=255; j>=0; j--) { // Ramp down from 255 to 0
-      strip_right.fill(strip_right.Color(0, 255, 0, strip_right.gamma8(j)));
+      strip_right.fill(strip_right.Color(0, 254, 0, strip_right.gamma8(j)));
       strip_right.show();
-      strip_left.fill(strip_left.Color(0, 255, 0, strip_left.gamma8(j)));
+      strip_left.fill(strip_left.Color(0, 254, 0, strip_left.gamma8(j)));
       strip_left.show();
     }
   }
